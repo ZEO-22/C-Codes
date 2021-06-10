5416f481edf306ba8f474ff644526023244ef27d
ZEO-22 <84535224+ZEO-22@users.noreply.github.com>
2021-06-10 19:56:10 +0100

    Create main.c

---
+++ TP2/Ex1/main.c
@@ -0,0 +1,36 @@
+  
+#include "functions.h"
+#include "functions.c"
+
+
+
+int main()
+{
+        t_point D ;
+	printf("il faut saisir les dimensions du tableau  \n");
+        saisie(&D);
+        affichage(D);
+	char ** t;
+	t = tab(D);
+	tab_saisie( D , t );
+	int i;
+	int j;
+	for(i=0 ; i<D.L ; i++)
+	{	
+		for(j=0 ; j<D.l ; j++)
+		{
+			printf(" %c ", t[i][j] );
+		}
+		printf("\n");
+	}
+	//vider(D , t);
+	t_point P;
+	printf("il faut saisir la position du point  \n");
+	P = get_point();
+	affichage(P);
+	char c;
+	char d;
+	printf("il faut saisir la valeur que vous voulez chager \n");
+	printf("il faut saisir la nouvelle valeur \n");
+        return 0;
+}
