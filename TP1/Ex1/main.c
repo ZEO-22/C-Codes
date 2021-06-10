7bc5cb626a473dee96403635df398338f2907a52
ZEO-22 <84535224+ZEO-22@users.noreply.github.com>
2021-06-10 19:44:12 +0100

    Create main.c

---
+++ TP1/Ex1/main.c
@@ -0,0 +1,19 @@
+include "functions.h"
+include "functions.c"
+
+
+int main()
+{
+  int n;
+  scanf("%d", &n);
+  // int result = recfib(n);
+  // printf("%d\n" , result);
+  // int result2 = fibo(n);
+  // printf("%d\n" , result2);
+  // int result3 = isprime(n);
+  // printf("%d\n" , result3);
+  // getprime(n);
+  int result5 = isprime(n);
+  printf("%d\n" , result5);
+  return 0;
+}
